362405f95b26f52fbd37e6f05ba4735ba850f8af
Gerwin Overeem <gerwin@overeem.io>
2019-01-10 16:58:28 +0100

    Vue + Bulma added

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          Hello World
+        </h1>
+        <p class="subtitle">
+          My first website with <strong>Bulma</strong>!
+        </p>
+        <SignupForm />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import SignupForm from './components/SignupForm.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SignupForm
+  }
+}
+</script>
---
+++ src/components/SignupForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="field">
+      <label class="label">Naam</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Text input" v-model="form.name">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Email</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input is-danger" type="email" placeholder="Email input" v-model="form.email">
+        <span class="icon is-small is-left">
+          <i class="fas fa-envelope"></i>
+        </span>
+        <span class="icon is-small is-right">
+          <i class="fas fa-exclamation-triangle"></i>
+        </span>
+      </div>
+      <p class="help is-danger">This email is invalid</p>
+    </div>
+
+    <div class="field">
+      <label class="label">Subject</label>
+      <div class="control">
+        <div class="select">
+          <select v-model="form.subject">
+            <option>Select dropdown</option>
+            <option>With options</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Message</label>
+      <div class="control">
+        <textarea class="textarea" placeholder="Textarea" v-model="form.textarea"></textarea>
+      </div>
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button class="button is-link">Submit</button>
+      </div>
+      <div class="control">
+        <button class="button is-text">Cancel</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignupForm',
+
+  data() {
+    return {
+      form: {
+        name: null,
+        email: null,
+        subject: null,
+        textarea: null
+      }
+    }
+  }
+}
+</script>
